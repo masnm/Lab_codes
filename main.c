d6aa75164f2c13fb4836a2ee4feb608de3f4cd53
masnm <nomanahmmed0@gmail.com>
2021-01-25 13:10:13 +0600

    Done...

--- main.c
+++ main.c
@@ -17,43 +17,8 @@ int min ( const void *a, const void *b ) {
 	return 0;
 } // void qsort ( void *ptr, size_t count, size_t size, int (*comp)(const void *, const void *) );
 
-void chmin ( int *a, int b ) { *a > b ? *a = b : false; }
-void chmax ( int *a, int b ) { *a < b ? *a = b : false; }
-
-typedef struct {
-	int min;
-	int max;
-	int s;
-} st ;
-
 int main ()
 {
-	int t,sum,l,r,ans; si(t);
-	char c[100005];
-	while ( t-- ) {
-		int n, m; si(n); si(m);
-		scanf("%s",c);
-		while ( m-- ) {
-			si(l); si(r);
-			int low = 0;
-			int high = 0;
-			sum = 0;
-			for ( int i = 0 ; i < l-1 ; i++ ) {
-				if ( c[i] == '-' ) sum--;
-				else sum++;
-				chmin ( &low, sum );
-				chmax ( &high, sum );
-			}
-			for ( int i = r ; i < n ; i++ ) {
-				if ( c[i] == '-' ) sum--;
-				else sum++;
-				chmin ( &low, sum );
-				chmax ( &high, sum );
-			}
-			ans = abs(low) + high + 1;
-			pi(ans); nln;
-		}
-	}
 
 
 	return 0;
