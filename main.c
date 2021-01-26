2fe509b53723d2b88a391753fd167d9db309aafd
masnm <nomanahmmed0@gmail.com>
2021-01-26 11:23:15 +0600

    task 1

--- main.c
+++ main.c
@@ -17,8 +17,28 @@ int min ( const void *a, const void *b ) {
 	return 0;
 } // void qsort ( void *ptr, size_t count, size_t size, int (*comp)(const void *, const void *) );
 
+int find_fib_sum ( int n )
+{
+	int sum = 0, first = 1, second = 1, next;
+	while ( first <= n ) {
+		printf ( "%d ", first );
+		sum += first;
+		next = first + second;
+		first = second;
+		second = next;
+	}
+	return sum;
+}
+
 int main ()
 {
+	int input;
+	printf("Enter the number : ");
+	scanf("%d",&input);
+
+	int sum = find_fib_sum ( input );
+
+	printf( "The sum is %d",sum );
 
 
 	return 0;
