a7c7caf2c0fdf688fdedad831eb5fe3d0b8fecaa
masnm <nomanahmmed0@gmail.com>
2021-02-28 19:30:36 +0600

    Lab 7 task 1

--- main.c
+++ main.c
@@ -12,27 +12,46 @@
 #define pis(a) printf("%lld ",a)
 #define nln printf("\n")
 
-#define chmax(a,b) if ( a < b ) a = b
-
+#define mx 10000005
+bool A[mx] = { };
 void prepare_lookup_table ()
 {
+	A[0] = A[1] = true;
+	int sq = (int)sqrt(mx); sq++;
+	for ( int i = 2 ; i < sq ; i++ ) {
+		if ( !A[i] ) {
+		       for ( int j = i*i ; j < mx ; j+=i ) {
+			       A[j] = true;
+		       }
+		}
+	}
+}
+
+void print_n_primes ( ll n )
+{
+	ll i = 1;
+	while ( n-- ) {
+		while ( A[i] ) {
+			i++;
+		}
+		pis(i); i++;
+	}
 }
 
 void do_task ()
 {
-	ll mx = INT_MIN, count = 1;
-	char c, pv = '.';
-	while ( scanf("%c",&c) != EOF ) {
-		if ( c != pv ) {
-			pv = c;
-			chmax ( mx, count );
-			count = 1;
-		} else {
-			count++;
-		}
+	printf("Enter a positive integer : ");
+	ll n; si(n);
+	if ( n < 1 ) {
+		puts("Invalid input.");
+		return;
 	}
-	if ( mx > 6 ) puts("YES");
-	else puts("NO");
+	if ( n > 664579 ) {
+		puts("Input is too high.");
+		return;
+	}
+	print_n_primes ( n );
+	nln;
 }
 
 int main ()
