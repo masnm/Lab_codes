ae1fbe3e46ee15aeeedacf058cc918d83f465c8e
masnm <nomanahmmed0@gmail.com>
2021-03-09 11:36:24 +0600

    mid task 7

--- main.c
+++ main.c
@@ -12,32 +12,31 @@
 #define pis(a) printf("%d ",a)
 #define nln printf("\n")
 
-bool arr[1005] = { };
+int fib_s[1001];
+int si,ei;
 void prepare_lookup_table ()
 {
-	arr[0] = arr[1] = true;
-	for ( int i = 2 ; i * i < 1005 ; ++i ) {
-		for ( int j = i*i ; j < 1005 ; j+=i ) {
-			arr[j] = true;
-		}
+	fib_s[0] = fib_s[1] = 1;
+	for ( int i = 2 ; fib_s[i-1]<1000 ; ++i ) {
+		fib_s[i] = fib_s[i-1] + fib_s[i-2];
+		if ( fib_s[i] < 100 ) ei = i;
+		if ( fib_s[i] < 1000 ) si = i;
 	}
 }
 
 void do_task ()
 {
-	printf("All coprime in range 100-1000 are : ");
-	for ( int i = 100 ; i < 1000-2 ; ++i ) {
-		if ( !arr[i] && !arr[i+2] ) {
-			pis(i); pis(i+2); nln;
-		}
+	for ( int i = si ; i >= ei ; --i ) {
+		pis(fib_s[i]);
 	}
+	nln;
 }
 
 int main ()
 {
-	prepare_lookup_table();
+	prepare_lookup_table ();
 
-	do_task();
+	do_task ();
 
 
 	return 0;
