8ecca91ae5981f17d8a891840cd0e2a3aa959244
masnm <nomanahmmed0@gmail.com>
2021-03-09 11:06:28 +0600

    mid task 5

--- main.c
+++ main.c
@@ -12,55 +12,18 @@
 #define pis(a) printf("%d ",a)
 #define nln printf("\n")
 
-void initialize ( int* a, int len, int ran )
+long double f ( float a )
 {
-	for ( int i = 0 ; i < len ; ++i ) {
-		a[i] = rand() % ran;
-		pis(a[i]);
-	}
-	nln;
-}
-
-int max ( int a, int b )
-{
-	return a>b?a:b;
+	return (a*a*a*a) + (a*a) + 1;
+	// return pow(a,4) + pow(a,2) + 1;
 }
 
 int main ()
 {
-	int x,y;
-	printf("Enter number X : "); si(x);
-	printf("Enter number Y : "); si(y);
-
-	int a[x], b[y];
-
-	printf("Arr1 : ");
-	initialize ( a, x, 10);
-	printf("Arr2 : ");
-	initialize ( b, y, 10);
-
-	int *pa = a+x-1, *pb = b+y-1;
-	int an_mx = max(x,y);
-	int ans[an_mx];
-	if ( x > y ) {
-		for ( int i = 0 ; i < x ; ++i ) {
-			ans[i] = *pa;
-			--pa;
-			ans[i] += pb<b?0:*pb;
-			--pb;
-		}
-	} else {
-		for ( int i = 0 ; i < y ; ++i ) {
-			ans[i] = *pb;
-			--pb;
-			ans[i] += pa<a?0:*pa;
-			--pa;
-		}
-	}
-
-	printf("Output : ");
-	for ( int i = an_mx-1 ; i > -1 ; --i ) {
-		pis(ans[i]);
+	float start = 0.01, end = 0.5;
+	while ( start <= end ) {
+		printf("For x = %.2f the value of f(x) = %Lf\n", start, f(start));
+		start += 0.01;
 	}
 
 
